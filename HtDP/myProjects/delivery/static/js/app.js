16c8f785b366986ac24b0350303af401b4db115b
BeAnMo <bamorin00@gmail.com>
2017-03-07 20:22:03 -0600

    Add files via upload

--- HtDP/myProjects/delivery/static/js/app.js
+++ HtDP/myProjects/delivery/static/js/app.js
@@ -1,1 +1,37 @@
-//
+window.onload = main;
+
+
+
+function main(){
+    console.log('loaded: app.js');
+    var btn = document.getElementById('info');
+    var display = document.getElementById('display');
+    var get = getData.bind(display);
+
+    btn.addEventListener('click', get);
+}
+
+function getData(event){
+    var req = new XMLHttpRequest();
+
+    req.open('GET', './info', true);
+    req.responseType = 'json';
+    //req.responseType = 'text';
+    
+    req.addEventListener('load', function(){
+	console.log('status: ' + req.status);
+	console.log(req.response);
+
+	displayData(display, req.response);
+    });
+
+    req.send(null);
+}
+
+function displayData(disp, res){
+    if(res === null){
+	disp.textContent = "No JSON output";
+    } else {
+	disp.textContent = "JSON: " + res['hello'];
+    }
+}
